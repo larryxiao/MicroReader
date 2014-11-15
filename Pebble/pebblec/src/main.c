ad50d057f490da4365bf0a23acfedb7a54d43112
Larry Xiao <xiaodi@sjtu.edu.cn>
2014-11-15 20:21:36 +0800

    [Pebble] high frequency update using AppTimerCallback

--- Pebble/pebblec/src/main.c
+++ Pebble/pebblec/src/main.c
@@ -3,7 +3,7 @@
   
 #define KEY_TEMPERATURE 0
 #define KEY_CONDITIONS 1
-  
+
 static Window *s_main_window;
 static TextLayer *s_word_layer;
 static TextLayer *s_status_layer;
@@ -17,7 +17,11 @@ static char * content;
 static int start_entry;
 static int next_word;
 
-static void update_time() {
+static bool flag = true;
+AppTimer *timer;
+static int wpm = 300;
+
+static void timer_callback() {
   // Get a tm structure
   time_t temp = time(NULL); 
   struct tm *tick_time = localtime(&temp);
@@ -63,10 +67,11 @@ static void update_time() {
                end_char++;
            }
     
-    buffer = (char*)malloc(sizeof(char)*(end_char - start_char));
+    buffer = (char*)malloc(sizeof(char)*(1 + end_char - start_char));
     for (char_index = start_char; char_index < end_char; char_index++) {
         buffer[char_index - start_char] = content[char_index];
     }
+    buffer[end_char - start_char] = 0;
 
   // split content to get clean word everytime it's called
 
@@ -76,127 +81,132 @@ static void update_time() {
   text_layer_set_text(s_word_layer, buffer);
     free(buffer);
     start_entry = end_char + 1;
+    // split content to get clean word everytime it's called
+
+    // strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+
+    timer = app_timer_register(60*1000/wpm, (AppTimerCallback) timer_callback, NULL);
 }
 
 static void main_window_load(Window *window) {
-  //Create GBitmap, then set to created BitmapLayer
-  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
-  s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
-  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
-  
-  // Create time TextLayer
-  s_word_layer = text_layer_create(GRect(3, 72, 139, 50));
-  text_layer_set_background_color(s_word_layer, GColorClear);
-  text_layer_set_text_color(s_word_layer, GColorWhite);
-  text_layer_set_text(s_word_layer, "Spribblealism");
-  
-  //Create GFont
-  s_word_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_16));
+    //Create GBitmap, then set to created BitmapLayer
+    s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+    s_background_layer = bitmap_layer_create(GRect(0, 0, 144, 168));
+    bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+    layer_add_child(window_get_root_layer(window), bitmap_layer_get_layer(s_background_layer));
 
-  //Apply to TextLayer
-  text_layer_set_font(s_word_layer, s_word_font);
-  text_layer_set_text_alignment(s_word_layer, GTextAlignmentCenter);
+    // Create time TextLayer
+    s_word_layer = text_layer_create(GRect(3, 72, 139, 50));
+    text_layer_set_background_color(s_word_layer, GColorClear);
+    text_layer_set_text_color(s_word_layer, GColorWhite);
+    text_layer_set_text(s_word_layer, "Spribblealism");
 
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_word_layer));
-  
-  // Create temperature Layer
-  s_status_layer = text_layer_create(GRect(0, 130, 144, 25));
-  text_layer_set_background_color(s_status_layer, GColorClear);
-  text_layer_set_text_color(s_status_layer, GColorWhite);
-  text_layer_set_text_alignment(s_status_layer, GTextAlignmentCenter);
-  text_layer_set_text(s_status_layer, "Loading...");
-  
-  // Create second custom font, apply it and add to Window
-  s_status_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
-  text_layer_set_font(s_status_layer, s_status_font);
-  layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_status_layer));
-  
-  // Make sure the time is displayed from the start
-  update_time();
+    //Create GFont
+    s_word_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_16));
+
+    //Apply to TextLayer
+    text_layer_set_font(s_word_layer, s_word_font);
+    text_layer_set_text_alignment(s_word_layer, GTextAlignmentCenter);
+
+    // Add it as a child layer to the Window's root layer
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_word_layer));
+
+    // Create temperature Layer
+    s_status_layer = text_layer_create(GRect(0, 130, 144, 25));
+    text_layer_set_background_color(s_status_layer, GColorClear);
+    text_layer_set_text_color(s_status_layer, GColorWhite);
+    text_layer_set_text_alignment(s_status_layer, GTextAlignmentCenter);
+    text_layer_set_text(s_status_layer, "Loading...");
+
+    // Create second custom font, apply it and add to Window
+    s_status_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
+    text_layer_set_font(s_status_layer, s_status_font);
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_status_layer));
+
+    // Make sure the time is displayed from the start
+    // update_time();
 }
 
 static void main_window_unload(Window *window) {
-  //Unload GFont
-  fonts_unload_custom_font(s_word_font);
-  
-  //Destroy GBitmap
-  gbitmap_destroy(s_background_bitmap);
+    //Unload GFont
+    fonts_unload_custom_font(s_word_font);
 
-  //Destroy BitmapLayer
-  bitmap_layer_destroy(s_background_layer);
-  
-  // Destroy TextLayer
-  text_layer_destroy(s_word_layer);
-  
-  // Destroy weather elements
-  text_layer_destroy(s_status_layer);
-  fonts_unload_custom_font(s_status_font);
+    //Destroy GBitmap
+    gbitmap_destroy(s_background_bitmap);
+
+    //Destroy BitmapLayer
+    bitmap_layer_destroy(s_background_layer);
+
+    // Destroy TextLayer
+    text_layer_destroy(s_word_layer);
+
+    // Destroy weather elements
+    text_layer_destroy(s_status_layer);
+    fonts_unload_custom_font(s_status_font);
 }
 
 static void tick_handler(struct tm *tick_time, TimeUnits units_changed) {
-  update_time();
-  
-  // Get weather update every 30 minutes
-  if(tick_time->tm_min % 30 == 0) {
-    // Begin dictionary
-    DictionaryIterator *iter;
-    app_message_outbox_begin(&iter);
+    //update_time();
 
-    // Add a key-value pair
-    dict_write_uint8(iter, 0, 0);
+    // Get weather update every 30 minutes
+    if(tick_time->tm_min % 30 == 0) {
+        // Begin dictionary
+        DictionaryIterator *iter;
+        app_message_outbox_begin(&iter);
 
-    // Send the message!
-    app_message_outbox_send();
-  }
+        // Add a key-value pair
+        dict_write_uint8(iter, 0, 0);
+
+        // Send the message!
+        app_message_outbox_send();
+    }
 }
 
 static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
-  // Store incoming information
-  static char temperature_buffer[8];
-  static char conditions_buffer[32];
-  static char weather_layer_buffer[32];
-  
-  // Read first item
-  Tuple *t = dict_read_first(iterator);
+    // Store incoming information
+    static char temperature_buffer[8];
+    static char conditions_buffer[32];
+    static char weather_layer_buffer[32];
 
-  // For all items
-  while(t != NULL) {
-    // Which key was received?
-    switch(t->key) {
-    case KEY_TEMPERATURE:
-      snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)t->value->int32);
-      break;
-    case KEY_CONDITIONS:
-      snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
-      break;
-    default:
-      APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
-      break;
+    // Read first item
+    Tuple *t = dict_read_first(iterator);
+
+    // For all items
+    while(t != NULL) {
+        // Which key was received?
+        switch(t->key) {
+            case KEY_TEMPERATURE:
+                snprintf(temperature_buffer, sizeof(temperature_buffer), "%dC", (int)t->value->int32);
+                break;
+            case KEY_CONDITIONS:
+                snprintf(conditions_buffer, sizeof(conditions_buffer), "%s", t->value->cstring);
+                break;
+            default:
+                APP_LOG(APP_LOG_LEVEL_ERROR, "Key %d not recognized!", (int)t->key);
+                break;
+        }
+
+        // Look for next item
+        t = dict_read_next(iterator);
     }
 
-    // Look for next item
-    t = dict_read_next(iterator);
-  }
-  
-  // Assemble full string and display
-  snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
-  text_layer_set_text(s_status_layer, weather_layer_buffer);
+    // Assemble full string and display
+    snprintf(weather_layer_buffer, sizeof(weather_layer_buffer), "%s, %s", temperature_buffer, conditions_buffer);
+    text_layer_set_text(s_status_layer, weather_layer_buffer);
 }
 
 static void inbox_dropped_callback(AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
 }
 
 static void outbox_failed_callback(DictionaryIterator *iterator, AppMessageResult reason, void *context) {
-  APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
+    APP_LOG(APP_LOG_LEVEL_ERROR, "Outbox send failed!");
 }
 
 static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
-  APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
+    APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
-  
+
 static void init() {
   // Spribble init
   content = "Hi, this is team spribble at HackShanghai!";
@@ -216,7 +226,8 @@ static void init() {
   window_stack_push(s_main_window, true);
   
   // Register with TickTimerService
-  tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
+  /*tick_timer_service_subscribe(SECOND_UNIT, tick_handler);*/
+  timer = app_timer_register(60*1000/wpm, (AppTimerCallback) timer_callback, NULL);
   
   // Register callbacks
   app_message_register_inbox_received(inbox_received_callback);
@@ -229,12 +240,12 @@ static void init() {
 }
 
 static void deinit() {
-  // Destroy Window
-  window_destroy(s_main_window);
+    // Destroy Window
+    window_destroy(s_main_window);
 }
 
 int main(void) {
-  init();
-  app_event_loop();
-  deinit();
+    init();
+    app_event_loop();
+    deinit();
 }
