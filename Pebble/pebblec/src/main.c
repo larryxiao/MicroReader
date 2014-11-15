00d8a14f198655a69a888002d202a53ed1cada11
Larry Xiao <xiaodi@sjtu.edu.cn>
2014-11-15 23:12:57 +0800

    [Pebble] button functions
    up / down single click vote up / down
    up / down multip click speed up / down

--- Pebble/pebblec/src/main.c
+++ Pebble/pebblec/src/main.c
@@ -1,6 +1,6 @@
 #include <pebble.h>
 #include <string.h>
-  
+
 #define KEY_TEMPERATURE 0
 #define KEY_CONDITIONS 1
 
@@ -18,16 +18,16 @@ static int start_entry;
 static int next_word;
 
 static bool flag = true;
-AppTimer *timer;
+static AppTimer *timer;
 static int wpm = 300;
 
 static void timer_callback() {
-  // Get a tm structure
-  time_t temp = time(NULL); 
-  struct tm *tick_time = localtime(&temp);
+    // Get a tm structure
+    time_t temp = time(NULL); 
+    struct tm *tick_time = localtime(&temp);
 
-  // Create a long-lived buffer
-  static char *buffer;
+    // Create a long-lived buffer
+    static char *buffer;
 
     int start_char = start_entry, end_char;
     int len = strlen(content);
@@ -35,50 +35,50 @@ static void timer_callback() {
     next_word = 0;
     while (start_char < len) {
         if ((content[start_char] <= '9' && content[start_char] >= '0')
-            || (content[start_char] <= 'z' && content[start_char] >= 'a')
-            || (content[start_char] <= 'Z' && content[start_char] >= 'A')
-            || (content[start_char] == '@')
-            || (content[start_char] == '#')
-            || (content[start_char] == '$')
-            || (content[start_char] == '%')
-            || (content[start_char] == '&')
-            || (content[start_char] == '\''))
+                || (content[start_char] <= 'z' && content[start_char] >= 'a')
+                || (content[start_char] <= 'Z' && content[start_char] >= 'A')
+                || (content[start_char] == '@')
+                || (content[start_char] == '#')
+                || (content[start_char] == '$')
+                || (content[start_char] == '%')
+                || (content[start_char] == '&')
+                || (content[start_char] == '\''))
         {
             next_word = 1;
             break;
         }
         start_char++;
     }
-    
+
     if(next_word == 0)
         return;
-    
+
     end_char = start_char;
     while (end_char < len &&
-           ((content[end_char] <= '9' && content[end_char] >= '0')
-            || (content[end_char] <= 'z' && content[end_char] >= 'a')
-            || (content[end_char] <= 'Z' && content[end_char] >= 'A')
-            || (content[end_char] == '@')
-            || (content[end_char] == '#')
-            || (content[end_char] == '$')
-            || (content[end_char] == '%')
-            || (content[end_char] == '&')
-            || (content[end_char] == '\''))) {
-               end_char++;
-           }
-    
+            ((content[end_char] <= '9' && content[end_char] >= '0')
+             || (content[end_char] <= 'z' && content[end_char] >= 'a')
+             || (content[end_char] <= 'Z' && content[end_char] >= 'A')
+             || (content[end_char] == '@')
+             || (content[end_char] == '#')
+             || (content[end_char] == '$')
+             || (content[end_char] == '%')
+             || (content[end_char] == '&')
+             || (content[end_char] == '\''))) {
+        end_char++;
+    }
+
     buffer = (char*)malloc(sizeof(char)*(1 + end_char - start_char));
     for (char_index = start_char; char_index < end_char; char_index++) {
         buffer[char_index - start_char] = content[char_index];
     }
     buffer[end_char - start_char] = 0;
 
-  // split content to get clean word everytime it's called
+    // split content to get clean word everytime it's called
 
-  // strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
+    // strftime(buffer, sizeof("00:00"), "%H:%M", tick_time);
 
-  // Display this time on the TextLayer
-  text_layer_set_text(s_word_layer, buffer);
+    // Display this time on the TextLayer
+    text_layer_set_text(s_word_layer, buffer);
     free(buffer);
     start_entry = end_char + 1;
     // split content to get clean word everytime it's called
@@ -103,9 +103,9 @@ static void main_window_load(Window *window) {
 
     //Create GFont
     s_word_font = 
-       // fonts_get_system_font("RESOURCE_ID_ROBOTO_CONDENSED_21")
-       // fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
-       fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SANSATION_20));
+        // fonts_get_system_font("RESOURCE_ID_ROBOTO_CONDENSED_21")
+        // fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
+        fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_SANSATION_20));
 
     //Apply to TextLayer
     text_layer_set_font(s_word_layer, s_word_font);
@@ -115,16 +115,16 @@ static void main_window_load(Window *window) {
     layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_word_layer));
 
     // Create temperature Layer
-    /*s_status_layer = text_layer_create(GRect(0, 130, 144, 25));*/
-    /*text_layer_set_background_color(s_status_layer, GColorClear);*/
-    /*text_layer_set_text_color(s_status_layer, GColorWhite);*/
-    /*text_layer_set_text_alignment(s_status_layer, GTextAlignmentCenter);*/
-    /*text_layer_set_text(s_status_layer, "Loading...");*/
+    s_status_layer = text_layer_create(GRect(0, 130, 144, 25));
+    text_layer_set_background_color(s_status_layer, GColorClear);
+    text_layer_set_text_color(s_status_layer, GColorWhite);
+    text_layer_set_text_alignment(s_status_layer, GTextAlignmentCenter);
+    text_layer_set_text(s_status_layer, "Loading...");
 
-    /*// Create second custom font, apply it and add to Window*/
-    /*s_status_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));*/
-    /*text_layer_set_font(s_status_layer, s_status_font);*/
-    /*layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_status_layer));*/
+    // Create second custom font, apply it and add to Window
+    s_status_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_20));
+    text_layer_set_font(s_status_layer, s_status_font);
+    //layer_add_child(window_get_root_layer(window), text_layer_get_layer(s_status_layer));
 
     // Make sure the time is displayed from the start
     // update_time();
@@ -210,36 +210,92 @@ static void outbox_sent_callback(DictionaryIterator *iterator, void *context) {
     APP_LOG(APP_LOG_LEVEL_INFO, "Outbox send success!");
 }
 
+static void up_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+    APP_LOG(APP_LOG_LEVEL_INFO, "up_single_click_handler success!");
+}
+
+static void down_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+    APP_LOG(APP_LOG_LEVEL_INFO, "down_single_click_handler success!");
+}
+
+// pass
+static void select_single_click_handler(ClickRecognizerRef recognizer, void *context) {
+    start_entry = 0;
+    APP_LOG(APP_LOG_LEVEL_INFO, "select_single_handler success!");
+}
+
+static void up_multi_click_handler(ClickRecognizerRef recognizer, void *context) {
+    wpm += 100;
+    if (wpm > 2000) wpm = 2000;
+    APP_LOG(APP_LOG_LEVEL_INFO, "up_multi_click_handler success!");
+}
+
+static void down_multi_click_handler(ClickRecognizerRef recognizer, void *context) {
+    wpm -= 100;
+    if (wpm < 100) wpm = 100;
+    APP_LOG(APP_LOG_LEVEL_INFO, "down_multi_click_handler success!");
+}
+
+// auto adjust speed
+static void select_long_click_handler(ClickRecognizerRef recognizer, void *context) {
+    APP_LOG(APP_LOG_LEVEL_INFO, "select_long_click_handler success!");
+}
+
+static void select_long_click_release_handler(ClickRecognizerRef recognizer, void *context) {
+    start_entry = 0;
+    APP_LOG(APP_LOG_LEVEL_INFO, "select_long_click_release_handler success!");
+}
+
+static void config_provider(Window *window) {
+    // single click / repeat-on-hold config:
+    window_single_click_subscribe(BUTTON_ID_UP, up_single_click_handler);
+    window_single_click_subscribe(BUTTON_ID_DOWN, down_single_click_handler);
+    window_single_click_subscribe(BUTTON_ID_SELECT, select_single_click_handler);
+
+    // multi click config:
+    /*window_multi_click_subscribe(BUTTON_ID_UP, 2, 10, 400, false, up_multi_click_handler);*/
+    /*window_multi_click_subscribe(BUTTON_ID_DOWN, 2, 10, 400, false, down_multi_click_handler);*/
+    window_multi_click_subscribe(BUTTON_ID_UP, 0, 0, 0, false, up_multi_click_handler);
+    window_multi_click_subscribe(BUTTON_ID_DOWN, 0, 0, 0, false, down_multi_click_handler);
+
+    // long click config:
+    window_long_click_subscribe(BUTTON_ID_SELECT, 700, select_long_click_handler, select_long_click_release_handler);
+
+}
+
 static void init() {
-  // Spribble init
-  content = "Hi, this is team PebbleReader at HackShanghai! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu consectetur eros. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse vulputate iaculis metus ut lacinia. Nunc dapibus elit in turpis euismod volutpat. Aenean magna neque, fringilla id dui eget, imperdiet volutpat justo. Aliquam ut diam malesuada nunc consectetur vulputate eget ut augue. Nullam lacinia vestibulum lacinia. Pellentesque nisl eros, elementum nec nibh a, efficitur elementum orci. Morbi dolor nisi, mattis sit amet velit eu, maximus tempus odio. Curabitur placerat pulvinar nisi. Nulla auctor tempor viverra. Vestibulum nec urna interdum, imperdiet quam vel, viverra enim. Aenean vitae metus nibh.";
-    
-  start_entry = 0;
+    // Spribble init
+    content = "Hi, this is team PebbleReader at HackShanghai! Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu consectetur eros. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Suspendisse vulputate iaculis metus ut lacinia. Nunc dapibus elit in turpis euismod volutpat. Aenean magna neque, fringilla id dui eget, imperdiet volutpat justo. Aliquam ut diam malesuada nunc consectetur vulputate eget ut augue. Nullam lacinia vestibulum lacinia. Pellentesque nisl eros, elementum nec nibh a, efficitur elementum orci. Morbi dolor nisi, mattis sit amet velit eu, maximus tempus odio. Curabitur placerat pulvinar nisi. Nulla auctor tempor viverra. Vestibulum nec urna interdum, imperdiet quam vel, viverra enim. Aenean vitae metus nibh.";
 
-  // Create main Window element and assign to pointer
-  s_main_window = window_create();
+    start_entry = 0;
 
-  // Set handlers to manage the elements inside the Window
-  window_set_window_handlers(s_main_window, (WindowHandlers) {
-    .load = main_window_load,
-    .unload = main_window_unload
-  });
+    // Create main Window element and assign to pointer
+    s_main_window = window_create();
 
-  // Show the Window on the watch, with animated=true
-  window_stack_push(s_main_window, true);
-  
-  // Register with TickTimerService
-  /*tick_timer_service_subscribe(SECOND_UNIT, tick_handler);*/
-  timer = app_timer_register(60*1000/wpm, (AppTimerCallback) timer_callback, NULL);
-  
-  // Register callbacks
-  app_message_register_inbox_received(inbox_received_callback);
-  app_message_register_inbox_dropped(inbox_dropped_callback);
-  app_message_register_outbox_failed(outbox_failed_callback);
-  app_message_register_outbox_sent(outbox_sent_callback);
-  
-  // Open AppMessage
-  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
+    // Set handlers to manage the elements inside the Window
+    window_set_window_handlers(s_main_window, (WindowHandlers) {
+            .load = main_window_load,
+            .unload = main_window_unload
+            });
+
+    // Set click config provider (as in example)
+    window_set_click_config_provider(s_main_window, (ClickConfigProvider) config_provider);
+
+    // Show the Window on the watch, with animated=true
+    window_stack_push(s_main_window, true);
+
+    // Register with TickTimerService
+    /*tick_timer_service_subscribe(SECOND_UNIT, tick_handler);*/
+    timer = app_timer_register(60*1000/wpm, (AppTimerCallback) timer_callback, NULL);
+
+    // Register callbacks
+    app_message_register_inbox_received(inbox_received_callback);
+    app_message_register_inbox_dropped(inbox_dropped_callback);
+    app_message_register_outbox_failed(outbox_failed_callback);
+    app_message_register_outbox_sent(outbox_sent_callback);
+
+    // Open AppMessage
+    app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
 
 static void deinit() {
